63b845d0ac8cdaa6d50f03f195bd2cc88a1c283f
pnkthrepwood <pnkthrepwood@gmail.com>
2019-01-26 13:14:43 +0100

    desiertito de fondo

--- gaem/main.cpp
+++ gaem/main.cpp
@@ -19,7 +19,10 @@ using namespace std;
 float RES_X = 1280.0f;
 float RES_Y = 720.0f;
 
+sf::Texture* tex_spritesheet;
 
+sf::Sprite spr_tile_dessert;
+const int TILE_SIZE = 16;
 
 int main()
 {
@@ -28,18 +31,22 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(RES_X, RES_Y), "SFML works!");
 	ImGui::SFML::Init(window);
 
-	sf::View view(sf::FloatRect(0.0f, 0.0f, RES_X*0.5f, RES_Y*0.5f));
+	sf::View cam(sf::FloatRect(0.0f, 0.0f, RES_X*0.5f, RES_Y*0.5f));
 	sf::View ui_view(sf::FloatRect(0.0f, 0.f, RES_X, RES_Y));
 
 	sf::Font font;
 	font.loadFromFile("8bitmadness.ttf");
 
+	tex_spritesheet = new sf::Texture();
+	tex_spritesheet->loadFromFile("sprite_sheet.png");
+	spr_tile_dessert.setTexture(*tex_spritesheet);
+	spr_tile_dessert.setTextureRect(sf::IntRect(1 * TILE_SIZE, 0, TILE_SIZE, TILE_SIZE));
 
 	sf::Clock clk_running;
 	sf::Clock clk_delta;
 	while (window.isOpen())
 	{
-		window.setView(view);
+		window.setView(cam);
 
 
 		sf::Event event;
@@ -67,7 +74,25 @@ int main()
 		window.setView(ui_view);
 		//Todo UI
 
-		window.setView(view);
+		window.setView(cam);
+
+		//Draw fondo del Mapita
+		int start_x = cam.getCenter().x - cam.getSize().x*0.5f - TILE_SIZE;
+		int start_y = cam.getCenter().y - cam.getSize().y*0.5f - TILE_SIZE;
+
+		int TILES_CAM_WIDTH = 100;
+		int TILES_CAM_HEIGHT = 100;
+
+		for (int x = start_x; x < start_x + TILES_CAM_WIDTH; ++x)
+		{
+			for (int y = start_y; y < start_y + TILES_CAM_HEIGHT; ++y)
+			{
+				spr_tile_dessert.setPosition(x*TILE_SIZE, y*TILE_SIZE);
+				window.draw(spr_tile_dessert);
+			}
+		}
+
+
 
 		ImGui::SFML::Render(window);
 
