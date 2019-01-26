b30e6f63443ae416e6658ff60cdfcad4d10a59e5
Pinkii- <pinkiibiza@gmail.com>
2019-01-26 13:48:31 +0100

    Add base shader for lights

--- gaem/main.cpp
+++ gaem/main.cpp
@@ -24,11 +24,42 @@ sf::Texture* tex_spritesheet;
 sf::Sprite spr_tile_dessert;
 const int TILE_SIZE = 16;
 
+
+sf::Shader nightLight;
+sf::VertexArray quad(sf::Quads, 4);
+
+void InitNightShader(sf::RenderTarget& renderTarget) {
+	nightLight.loadFromFile("nightLight.frag", sf::Shader::Type::Fragment);
+
+	auto size = renderTarget.getSize();
+	quad[0].position = sf::Vector2f(0,0);
+	quad[1].position = sf::Vector2f(0,size.y);
+	quad[2].position = sf::Vector2f(size.x,size.y);
+	quad[3].position = sf::Vector2f(size.x,0);
+
+	quad[0].texCoords = sf::Vector2f(0,0);
+	quad[1].texCoords = sf::Vector2f(0,size.y);
+	quad[2].texCoords = sf::Vector2f(size.x,size.y);
+	quad[3].texCoords = sf::Vector2f(size.x,0);
+}
+
+void RenderWithShader(sf::RenderWindow& window, const sf::RenderTexture& renderTexture) {
+	sf::RenderStates states;
+	window.clear();
+	nightLight.setUniform("texture", sf::Shader::CurrentTexture);
+	states.shader = &nightLight;
+	states.texture = &renderTexture.getTexture();
+	window.draw(quad, states);
+	window.display();
+}
+
 int main()
 {
 	srand(time(NULL));
 
 	sf::RenderWindow window(sf::VideoMode(RES_X, RES_Y), "SFML works!");
+	sf::RenderTexture renderTexture;
+	renderTexture.create(RES_X, RES_Y);
 	ImGui::SFML::Init(window);
 
 	sf::View cam(sf::FloatRect(0.0f, 0.0f, RES_X*0.5f, RES_Y*0.5f));
@@ -42,11 +73,13 @@ int main()
 	spr_tile_dessert.setTexture(*tex_spritesheet);
 	spr_tile_dessert.setTextureRect(sf::IntRect(1 * TILE_SIZE, 0, TILE_SIZE, TILE_SIZE));
 
+	InitNightShader(window);
+
 	sf::Clock clk_running;
 	sf::Clock clk_delta;
 	while (window.isOpen())
 	{
-		window.setView(cam);
+		renderTexture.setView(cam);
 
 
 		sf::Event event;
@@ -68,13 +101,13 @@ int main()
 		GamePad::UpdateInputState();
 
 
-		window.clear();
+		renderTexture.clear();
 
 
-		window.setView(ui_view);
+		renderTexture.setView(ui_view);
 		//Todo UI
 
-		window.setView(cam);
+		renderTexture.setView(cam);
 
 		//Draw fondo del Mapita
 		int start_x = cam.getCenter().x - cam.getSize().x*0.5f - TILE_SIZE;
@@ -88,15 +121,18 @@ int main()
 			for (int y = start_y; y < start_y + TILES_CAM_HEIGHT; ++y)
 			{
 				spr_tile_dessert.setPosition(x*TILE_SIZE, y*TILE_SIZE);
-				window.draw(spr_tile_dessert);
+				renderTexture.draw(spr_tile_dessert);
 			}
 		}
 
 
 
+		renderTexture.display();
+
 		ImGui::SFML::Render(window);
 
-		window.display();
+		RenderWithShader(window, renderTexture);
+
 	}
 
 	return 0;
