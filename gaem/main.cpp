346a7597f9c69634061617008dc5211ac907a55c
Albert Vaca <albertvaka@gmail.com>
2019-01-27 12:15:03 +0100

    Danger music

--- gaem/main.cpp
+++ gaem/main.cpp
@@ -56,7 +56,7 @@ sf::SoundBuffer* bchopwood; //done
 sf::Font* font;
 
 sf::Music* music; //Done
-sf::Music* musicdanger;
+sf::Music* musicdanger; //done
 
 sf::Shader* nightLight;
 sf::VertexArray quad(sf::Quads, 4);
@@ -739,9 +739,12 @@ int main()
 	InitPlayers();
 
 	music->play();
+	music->setLoop(true);
+	musicdanger->play();
+	musicdanger->setVolume(0);
+	musicdanger->setLoop(true);
 
 	obj_manager.Spawn(GameObjectType::WATER, RES_X/2, RES_Y / 2);
-
 
 	auto p = GetCasillaFromCam(cam);
 	current_casilla_x = p.first;
@@ -806,10 +809,11 @@ int main()
 		}
 
 		//Enemies
+		bool in_danger = false;
 		for (int i = 0; i < enemies.size();)
 		{
 			bool cale_destruir = enemies[i]->Update(dt_time.asSeconds());
-
+			if (enemies[i]->state == PlayerState::WALKING) in_danger = true;
 			if (cale_destruir)
 			{
 				delete enemies[i];
@@ -819,6 +823,12 @@ int main()
 			{
 				i++;
 			}
+		}
+		if (in_danger) {
+			musicdanger->setVolume(100);
+		}
+		else {
+			musicdanger->setVolume(0);
 		}
 		//Bullets
 		for (int i = 0; i < bullets.size();) {
@@ -901,7 +911,7 @@ int main()
 		//Enemies
 		for (int i = 0; i < enemies.size(); i++)
 		{
-			int frame = static_cast<int>(enemies[i]->anim_timer / 0.2f) % 2;
+			int frame = static_cast<int>(enemies[i]->anim_timer / 0.1f) % 2;
 
 			sf::Sprite * sprite = enemies[i]->sprite;
 
